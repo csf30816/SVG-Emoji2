5158f0ab7ed84ca3added5f1a25ff2caefccef79
csf30816 <csf30816@users.noreply.github.com>
2017-07-14 17:05:23 -1000

    Update emojify.js

--- src/emojify.js
+++ src/emojify.js
@@ -1,3 +1,5 @@
-var emojify = function () {
-
+var emojify = function (text) {
+  var newText = text.replace(':)', '<span style="background-size: 100% 100%; background-image: url(\'http://tinyurl.com/svg-smile\');">&nbsp&nbsp&nbsp&nbsp</span>');
+  newText = text.replace(':D', '<span style="background-size: 100% 100%; background-image: url(\'http://tinyurl.com/yaqdh78e\');">&nbsp&nbsp&nbsp&nbsp</span>');
+  return newText;
 }
