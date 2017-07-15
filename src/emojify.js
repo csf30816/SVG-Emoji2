a71aa0f846a5227e385834cb6c04d73fa72e41f6
csf30816 <csf30816@users.noreply.github.com>
2017-07-14 17:51:35 -1000

    Update emojify.js

--- src/emojify.js
+++ src/emojify.js
@@ -1,6 +1,6 @@
 var emojify = function (text) {
-  var newText = text.replace(':)', '<span style="background-size: 100% 100%; background-image: url(\'http://tinyurl.com/svg-smile\');">&nbsp&nbsp&nbsp&nbsp</span>');
-  newText = newText.replace(':D', '<span style="background-size: 100% 100%; background-image: url(\'http://tinyurl.com/yaqdh78e\');">&nbsp&nbsp&nbsp&nbsp</span>');
+  var newText = text.replace(':)', '<span style="background-size: 100% 100%; background-image: url(\'//tinyurl.com/svg-smile\');">&nbsp&nbsp&nbsp&nbsp</span>');
+  newText = newText.replace(':D', '<span style="background-size: 100% 100%; background-image: url(\'//tinyurl.com/yaqdh78e\');">&nbsp&nbsp&nbsp&nbsp</span>');
   return newText;
 }
 
@@ -12,6 +12,7 @@ setInterval(
               document.body.innerHTML.search(":D");
     if (emojis > -1) {
       document.body.innerHTML = emojify(document.body.innerHTML);
+      console.clear(); // Just so sites with lot's of errors don't overflow ;)
     }
   }, 100
 );
