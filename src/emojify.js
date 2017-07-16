6a280879bacf83a5b0367142705911397d46d8ac
csf30816 <csf30816@users.noreply.github.com>
2017-07-15 17:22:10 -1000

    Update emojify.js

--- src/emojify.js
+++ src/emojify.js
@@ -1,36 +1,81 @@
 var emojify = function (text) {
-  // Smile
+
+  // Smiling Slightly
   var newText = text.replace(':)', '<span style="background-size: 100% 100%; background-image: url(\'https://csf30816.github.io/SVG-Emoji2/emojis/smile.svg\');">&nbsp&nbsp&nbsp&nbsp</span>');
-  newText = newText.replace(':smile:', '<span style="background-size: 100% 100%; background-image: url(\'https://csf30816.github.io/SVG-Emoji2/emojis/smile.svg\');">&nbsp&nbsp&nbsp&nbsp</span>');
+
+  newText = newText.replace(':slight_smile:', '<span style="background-size: 100% 100%; background-image: url(\'https://csf30816.github.io/SVG-Emoji2/emojis/smile.svg\');">&nbsp&nbsp&nbsp&nbsp</span>');
+
+  newText = newText.replace('🙂', '<span style="background-size: 100% 100%; background-image: url(\'https://csf30816.github.io/SVG-Emoji2/emojis/smile.svg\');">&nbsp&nbsp&nbsp&nbsp</span>');
+
   
-  // Big Smile
+
+  // Grinning
+
   newText = newText.replace(':D', '<span style="background-size: 100% 100%; background-image: url(\'https://csf30816.github.io/SVG-Emoji2/emojis/smile-big.svg\');">&nbsp&nbsp&nbsp&nbsp</span>');
-  newText = newText.replace(':big-smile:', '<span style="background-size: 100% 100%; background-image: url(\'https://csf30816.github.io/SVG-Emoji2/emojis/smile-big.svg\');">&nbsp&nbsp&nbsp&nbsp</span>');
+
+  newText = newText.replace(':grinning:', '<span style="background-size: 100% 100%; background-image: url(\'https://csf30816.github.io/SVG-Emoji2/emojis/smile-big.svg\');">&nbsp&nbsp&nbsp&nbsp</span>');
+  newText = newText.replace('😀', '<span style="background-size: 100% 100%; background-image: url(\'https://csf30816.github.io/SVG-Emoji2/emojis/smile-big.svg\');">&nbsp&nbsp&nbsp&nbsp</span>');
+
   
-  // Frown
+
+  // Frowning Slightly
+
   newText = newText.replace(':(', '<span style="background-size: 100% 100%; background-image: url(\'https://csf30816.github.io/SVG-Emoji2/emojis/frown.svg\');">&nbsp&nbsp&nbsp&nbsp</span>');
-  newText = newText.replace(':frown:', '<span style="background-size: 100% 100%; background-image: url(\'https://csf30816.github.io/SVG-Emoji2/emojis/frown.svg\');">&nbsp&nbsp&nbsp&nbsp</span>');
+
+  newText = newText.replace(':frowning2:', '<span style="background-size: 100% 100%; background-image: url(\'https://csf30816.github.io/SVG-Emoji2/emojis/frown.svg\');">&nbsp&nbsp&nbsp&nbsp</span>');
+
+  newText = newText.replace('☹', '<span style="background-size: 100% 100%; background-image: url(\'https://csf30816.github.io/SVG-Emoji2/emojis/frown.svg\');">&nbsp&nbsp&nbsp&nbsp</span>');
+
   
-  // Big Frown
-  newText = newText.replace(':big-frown:', '<span style="background-size: 100% 100%; background-image: url(\'https://csf30816.github.io/SVG-Emoji2/emojis/frown-big.svg\');">&nbsp&nbsp&nbsp&nbsp</span>');
+
+  // Frowning With Open Mouth
+
+  newText = newText.replace(':frowning:', '<span style="background-size: 100% 100%; background-image: url(\'https://csf30816.github.io/SVG-Emoji2/emojis/frown-big.svg\');">&nbsp&nbsp&nbsp&nbsp</span>');
+
+  newText = newText.replace('😦', '<span style="background-size: 100% 100%; background-image: url(\'https://csf30816.github.io/SVG-Emoji2/emojis/frown-big.svg\');">&nbsp&nbsp&nbsp&nbsp</span>');
+
   
+// Frowning With Open Mouth
+
+  newText = newText.replace(':frowning:', '<span style="background-size: 100% 100%; background-image: url(\'https://csf30816.github.io/SVG-Emoji2/emojis/frown-big.svg\');">&nbsp&nbsp&nbsp&nbsp</span>');
+
+  newText = newText.replace('😦', '<span style="background-size: 100% 100%; background-image: url(\'https://csf30816.github.io/SVG-Emoji2/emojis/frown-big.svg\');">&nbsp&nbsp&nbsp&nbsp</span>');
+
+  
+
   return newText;
+
 }
-
 var emojis = -1;
-
 setInterval(
   function () {
     emojis =  document.body.innerHTML.search(":\\)") +
               document.body.innerHTML.search(":\\(") +
-              document.body.innerHTML.search(":smile:") +
-              document.body.innerHTML.search(":big-smile") +
-              document.body.innerHTML.search(":frown:") +
-              document.body.innerHTML.search(":big-frown") +
+              document.body.innerHTML.search(":slight-smile:") +
+              document.body.innerHTML.search("🙂") +
+
+              document.body.innerHTML.search(":grinning:") +
+
+              document.body.innerHTML.search("😀") +
+
+              document.body.innerHTML.search(":frowning2:") +
+
+              document.body.innerHTML.search("☹") +
+
+              document.body.innerHTML.search(":frowning:") +
+
+              document.body.innerHTML.search("😦") +
+
               document.body.innerHTML.search(":D");
+
     if (emojis > -1) {
+
       document.body.innerHTML = emojify(document.body.innerHTML);
+
       console.clear(); // Just so sites with lot's of errors don't overflow ;)
+
     }
+
   }, 500
+
 );
