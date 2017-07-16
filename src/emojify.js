37984c10cb0caf264ee5dee149ade7f78d9106d5
csf30816 <csf30816@users.noreply.github.com>
2017-07-15 17:32:12 -1000

    Update emojify.js

--- src/emojify.js
+++ src/emojify.js
@@ -20,24 +20,29 @@ var emojify = function(text) {
 	newText = newText.replace('😦', '<span style="background-size: 100% 100%; background-image: url(\'https://csf30816.github.io/SVG-Emoji2/emojis/frown-big.svg\');">&nbsp&nbsp&nbsp&nbsp</span>');
 
 	// Frowning With Open Mouth
-	newText = newText.replace(':frowning:', '<span style="background-size: 100% 100%; background-image: url(\'https://csf30816.github.io/SVG-Emoji2/emojis/frown-big.svg\');">&nbsp&nbsp&nbsp&nbsp</span>');
-	newText = newText.replace('😦', '<span style="background-size: 100% 100%; background-image: url(\'https://csf30816.github.io/SVG-Emoji2/emojis/frown-big.svg\');">&nbsp&nbsp&nbsp&nbsp</span>');
+	newText = newText.replace(':open_mouth:', '<span style="background-size: 100% 100%; background-image: url(\'https://csf30816.github.io/SVG-Emoji2/emojis/open-mouth.svg\');">&nbsp&nbsp&nbsp&nbsp</span>');
+	newText = newText.replace('😮', '<span style="background-size: 100% 100%; background-image: url(\'https://csf30816.github.io/SVG-Emoji2/emojis/open-mouth.svg\');">&nbsp&nbsp&nbsp&nbsp</span>');
+	newText = newText.replace(':O', '<span style="background-size: 100% 100%; background-image: url(\'https://csf30816.github.io/SVG-Emoji2/emojis/open-mouth.svg\');">&nbsp&nbsp&nbsp&nbsp</span>');
+	
 	return newText;
 }
 var emojis = -1;
 setInterval(
 	function() {
 		emojis = document.body.innerHTML.search(":\\)") +
-			document.body.innerHTML.search(":\\(") +
-			document.body.innerHTML.search(":slight-smile:") +
-			document.body.innerHTML.search("🙂") +
-			document.body.innerHTML.search(":grinning:") +
-			document.body.innerHTML.search("😀") +
-			document.body.innerHTML.search(":frowning2:") +
-			document.body.innerHTML.search("☹") +
-			document.body.innerHTML.search(":frowning:") +
-			document.body.innerHTML.search("😦") +
-			document.body.innerHTML.search(":D");
+			 document.body.innerHTML.search(":\\(") +
+			 document.body.innerHTML.search(":slight_smile:") +
+			 document.body.innerHTML.search("🙂") +
+			 document.body.innerHTML.search(":grinning:") +
+			 document.body.innerHTML.search("😀") +
+			 document.body.innerHTML.search(":frowning2:") +
+			 document.body.innerHTML.search("☹") +
+			 document.body.innerHTML.search(":frowning:") +
+			 document.body.innerHTML.search("😦") +
+			 document.body.innerHTML.search(":open_mouth:") +
+			 document.body.innerHTML.search("😮") +
+			 document.body.innerHTML.search(":O") +
+			 document.body.innerHTML.search(":D");
 
 		if (emojis > -1) {
 			document.body.innerHTML = emojify(document.body.innerHTML);
